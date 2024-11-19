44d7e723f73b7e2996ca24c0cc2150839b1406e2
Alec Thilenius <alec@thilenius.com>
2024-11-18 20:20:29 -0800

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  /* @type { import('./$houdini').PageData } */
+  export let data;
+
+  $: ({ Countries } = data);
+</script>
+
+<pre>{JSON.stringify($Countries.data.countries, null, 2)}</pre>
